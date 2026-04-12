c693f8f2eb8be07ca2756cb54edff557de2cc42e
n-gintetsu <info@gintetsu-fudosan.co.jp>
2026-04-12 12:01:51 +0900

    EstateFlowにGoogle Analytics GA4タグ追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-9GL11H1C10" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-9GL11H1C10');
+      `}</Script>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
