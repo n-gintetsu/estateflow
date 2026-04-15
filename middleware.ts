5619697531080cc1811f82152204380aa1866343
n-gintetsu <info@gintetsu-fudosan.co.jp>
2026-04-15 09:48:47 +0900

    middleware 一時無効化（各ページで認証チェック）

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get('sb-cnhafquczeoxarliruvg-auth-token')
-  const token2 = req.cookies.get('sb-cnhafquczeoxarliruvg-auth-token.0')
-  const token3 = req.cookies.get('sb-cnhafquczeoxarliruvg-auth-token.1')
-
-  if (!token && !token2 && !token3) {
-    return NextResponse.redirect(new URL('/login', req.url))
-  }
-
   return NextResponse.next()
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|api|login|reset-password|update-password).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|api).*)'],
 }
