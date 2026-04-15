77ba6abe71589d0b233076f46961e0f31b9f0daf
n-gintetsu <info@gintetsu-fudosan.co.jp>
2026-04-15 09:22:04 +0900

    middleware クッキーベース認証に変更

--- middleware.ts
+++ middleware.ts
@@ -1,33 +1,15 @@
-import { createServerClient } from '@supabase/ssr'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function middleware(req: NextRequest) {
-  const res = NextResponse.next()
+  const token = req.cookies.get('sb-cnhafquczeoxarliruvg-auth-token')
+  const token2 = req.cookies.get('sb-cnhafquczeoxarliruvg-auth-token.0')
+  const token3 = req.cookies.get('sb-cnhafquczeoxarliruvg-auth-token.1')
 
-  const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return req.cookies.getAll()
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => {
-            res.cookies.set(name, value, options)
-          })
-        },
-      },
-    }
-  )
-
-  const { data: { user } } = await supabase.auth.getUser()
-
-  if (!user) {
+  if (!token && !token2 && !token3) {
     return NextResponse.redirect(new URL('/login', req.url))
   }
 
-  return res
+  return NextResponse.next()
 }
 
 export const config = {
